3dcd50d05146eafceb97f01351cf8d7d2f55671b
Erik Barroso <coderik1996@gmail.com>
2024-01-09 17:20:59 -0300

    starting test

---
+++ test/api.test.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+test("Deve comprar um ingresso", async function () {
+  const input = {
+    eventId: "3052b3fe-e3ec-482f-bf67-449132e3429f",
+    email: "teste.teste@gmail.com",
+    creditCardToken: "958654312",
+  };
+  const response = await axios.post(
+    "http://localhost:3000/purchase_ticket",
+    input
+  );
+  const output = response.data;
+
+  expect(output.status).toBe("paid");
+});
